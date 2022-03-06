643984589b8b32b712c9a7784476cba590791f9b
wqicheng <wqicheng@foxmail.com>
2022-03-07 07:53:45 +0800

    2022年 03月 07日 星期一 07:53:45 CST:修复bug

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-desc="$(cat ./desc)"
+read desc
 git add .
-git commit -m "$(date)\n$desc"
+git commit -m "$(date):$desc"
 git remote add origin git@github.com:amwqicheng/amwqicheng.github.io.git
 git push -u origin main
